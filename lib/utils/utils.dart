59fc5ad7c355f225ce6c11d6d9887cbe64e34c26
MohammadHossein Misaghpour <misagh1999@gmail.com>
2022-07-09 13:16:03 +0430

    changed update api

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
 import '../widgets/dialog.dart';
+import 'package:get/get.dart';
 
 String replacePersianNum(String input) {
   const english = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
@@ -30,26 +32,28 @@ getVersionNumber() async {
 }
 
 showUpdateDialogIfAvailable() async {
-  QueryBuilder<ParseObject> queryVersion =
-      QueryBuilder<ParseObject>(ParseObject('AppVersion'));
-  final ParseResponse apiResponse = await queryVersion.query();
-  if (apiResponse.success && apiResponse.result != null) {
-    PackageInfo packageInfo = await PackageInfo.fromPlatform();
-    final packageName = packageInfo.packageName;
-    final currentVersion = int.parse(packageInfo.buildNumber);
-    final appVersions = apiResponse.results as List<ParseObject>;
-    int lastVersion = -1;
-    for (var app in appVersions) {
-      if (app["package_name"] as String == packageName) {
-        lastVersion = app["version_number"] as int;
-        break;
+  final hasUpdate = await MyBackendApi().isUpdateAvailable();
+  if (hasUpdate) {
+    showUpdateDialog();
+  }
+}
+
+class MyBackendApi extends GetConnect {
+  Future<bool> isUpdateAvailable() async {
+    final path = 'https://hobby.misaghpour-dev.ir/version';
+    final response = await get(path);
+    if (response.status.hasError) {
+      return false;
+    } else {
+      final result = response.body;
+      PackageInfo packageInfo = await PackageInfo.fromPlatform();
+      final currentVersion = int.parse(packageInfo.buildNumber);
+      final lastVersionCode = result['last_version_code'] as int;
+      if (lastVersionCode > currentVersion) {
+        return true;
+      } else {
+        return false;
       }
     }
-    if (lastVersion > currentVersion) {
-      showUpdateDialog();
-      print('Show Dialog');
-    }
-  } else {
-    print('error');
   }
 }
