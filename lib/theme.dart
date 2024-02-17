f29423990eea768c775ad07a91d6ddf48fe21a71
Mohammadhossein Misaghpour <misagh1999@gamil.com>
2024-02-17 11:41:11 +0330

    changed backgroundColor to white

--- lib/theme.dart
+++ lib/theme.dart
@@ -5,6 +5,7 @@ import 'constants.dart';
 ThemeData lightThemeData(BuildContext context) {
   return ThemeData(
       fontFamily: Fonts.Regular,
+      scaffoldBackgroundColor: Colors.white,
       colorScheme: ColorScheme.fromSwatch()
           .copyWith(secondary: secondaryColor, primary: primaryColor));
 }
